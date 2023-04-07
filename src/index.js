413fecea9ff844dae4bef37891e2d5fa362fa754
Darley Peralta <ordeal00s@gmail.com>
2023-04-07 19:47:26 -0400

    dp

--- src/index.js
+++ src/index.js
@@ -1,3 +1,79 @@
+
 import './less/index.less'
 
-// Your code goes here!
+// Your code goes here!//
+
+// load
+window.onload = function (evt) {
+    console.log(`event ${evt.type} fired, Ready to go!`);
+    const heading = document.querySelector('h1');
+    heading.textContent = "Ready TO GO"
+
+
+
+
+// copy
+window.addEventListener('copy', ()=>{
+    navigator.clipboard.readText()
+    .then(text => {
+        console.log(text)
+    })
+})
+
+
+// click
+document.body.addEventListener('click', evt => {
+    evt.target.classList.toggle('mirror')
+})
+// 
+// double click
+
+document.body.addEventListener('dblclick', evt => {
+
+    // evt.target.outerHTML = ''
+})
+
+// keydown
+
+window.addEventListener('keydown', evt => {
+    if(evt.key == 6){
+        document.body.innerHTML = 'fu'
+    }
+})
+
+//mouse move 
+document.body.addEventListener('mousemove', evt => {
+    const {clientX, clientY} = evt
+    // console.log(`mouse is at ${clientX}, ${clientY}`)
+
+})
+
+
+//mouse enter
+//mosue leave 
+
+const des = document.querySelectorAll('.destination')
+console.log(des)
+for ( let dest of des){
+    dest.addEventListener('mouseenter', evt => {
+        dest.style.fontWeight = 'bold'
+    })
+    dest.addEventListener('mouseleave', evt => {
+        dest.style.fontWeight = 'initial'
+    })
+}
+
+ //cut 
+
+document.body.addEventListener('cut', evt => {
+    evt.target.style.color = "red"
+})
+
+
+window.addEventListener('keyup', evt => {
+    if(evt.key == 'd'){
+        document.body.innerHTML = 'fu'
+    }
+})
+
+}
